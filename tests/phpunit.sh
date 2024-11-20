dc210cecf4f7fc305fa9ca19ae7d6b0e2b8a4ac2
usk845u189 <uikousuke315@gmail.com>
2024-11-20 19:01:56 +0900

    trysomething for test

--- tests/phpunit.sh
+++ tests/phpunit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker exec -it web-app php artisan config:clear
-# docker exec web-app php artisan migrate:fresh --seed --database=local_test
-docker exec web-app php artisan migrate --seed --database=local_test
+# docker exec web-app php artisan migrate:fresh --seed --database=test
+docker exec web-app php artisan migrate --seed --database=test
 docker exec -it web-app vendor/bin/phpunit
